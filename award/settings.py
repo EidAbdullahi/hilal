94c4180f00bd2b69227d12f660e2a4352108b509
eid <eidabdullahi10@gmail.com>
2021-05-04 18:01:31 +0300

    commit changes

--- award/settings.py
+++ award/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY =('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # .....?
 # ......
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
